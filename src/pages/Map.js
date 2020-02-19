0528bb10b9a89c08b1a74b81071c1497fd24dfd8
Zihan Wei <zihan66@tamu.edu>
2020-02-19 10:26:56 -0600

    start navigation

---
+++ src/pages/Map.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Login extends Component {
+    render(){
+      return(
+         <View style={styles.container}>
+            <Text>This is a map</Text>
+         </View>
+      )
+    }
+  }
+
+  const styles = StyleSheet.create({
+      container:{
+
+      }
+  })
