62d4580c4d8a14fd984b47c8abd35bf4e06ae843
MaomiHz <maomihz@gmail.com>
2020-02-19 08:49:23 -0600

    improve hello world api

--- friend_router/resources/helloworld.py
+++ friend_router/resources/helloworld.py
@@ -1,9 +1,34 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse, fields, marshal_with
+
+class Hello:
+    def __init__(self, name, greeting):
+        self.name = name
+        self.greeting = greeting
+
+    @property
+    def modern(self):
+        return '{}, {}!'.format(self.greeting.capitalize(), self.name.capitalize())
+
+    @property
+    def traditional(self):
+        return '{}, {}'.format(self.greeting.lower(), self.name.lower())
+
+    @property
+    def original(self):
+        return '{}, {}'.format(self.greeting, self.name)
+
+hello_fields = {
+    'hello': fields.String(attribute='name'),
+    'modern': fields.String(),
+    'traditional': fields.String(),
+    'original': fields.String()
+}
+
+parser = reqparse.RequestParser()
+parser.add_argument('greeting', default='hello')
 
 class HelloWorld(Resource):
+    @marshal_with(hello_fields)
     def get(self, name='world'):
-        return {
-            'hello': name,
-            'modern': 'Hello, {}!'.format(name.capitalize()),
-            'traditional': 'hello, {}'.format(name.lower())
-        }
+        args = parser.parse_args()
+        return Hello(name, args['greeting'])
