46dd4790242a20b14c1a2cd60f8b312b013f63bd
Fengqiao Wang <fengqiao-wang@tamu.edu>
2020-03-01 14:09:39 -0600

    User location

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
-import React, { Component } from 'react';
-import {StyleSheet, Text, View, StatusBar} from 'react-native';
-import Login from './src/pages/Login'
-export default class App extends Component{
+import React from 'react';
+import MapView from 'react-native-maps';
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+export default class App extends React.Component {
+
+    state = {
+      latitude: null,
+      longitude: null
+    }
+
+  async componentDidMount(){
+
+    const {status} = await Permissions.getAsync(Permissions.LOCATION)
+
+    if (status !== 'granted'){
+      const response = await Permissions.askAsync(Permissions.LOCATION)
+    }
+
+
+
+    navigator.geolocation.getCurrentPosition(
+    ({coords : {latitude, longitude} }) => this.setState({latitude, longitude}, () => console.log('State:', this.state)),
+
+    (error) => console.log('Error', error)
+    )
+    
+  }
+
+
   render() {
+     const latitude = this.state.latitude
+     const longitude = this.state.longitude
     return (
       <View style={styles.container}>
-        <StatusBar backgroundColor='#5d99c6'
-        barStyle='light-content'
-        />
-        <Login/>
+        <MapView style={styles.mapStyle} 
+        showsUserLocation={true} 
+        showsMyLocationButton={true}
+        initialRegion={{
+          latitude: 30.622370,
+          longitude: -96.325851,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        >
+        </MapView>
       </View>
     );
   }
@@ -16,9 +53,13 @@ export default class App extends Component{
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    backgroundColor: '#90caf9',
+    flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
