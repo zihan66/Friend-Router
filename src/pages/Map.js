d57053149cb9ee79202327fd86b4234aacc7b58c
Zihan Wei <zihan66@tamu.edu>
2020-03-02 01:07:28 -0600

    add marker and send location to the server

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,11 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { Component } from 'react';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import {View, StyleSheet, Dimensions } from 'react-native';
 
-export default class Map extends React.Component {
+
+export default class Map extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      region: {
+      latitude: 30.622370,
+      longitude: -96.325851,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421},
+      location: null
+    }
+
+    this._getLocationAsync();
+    this.sendLocation();
+  }
+
+
+
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if(status !== 'granted')
+        console.log('Permission to access was denied.')
+
+    let location = await Location.getCurrentPositionAsync({enabledHighAccuracy: true});
+    let region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.045
+    }
+    this.setState({ region: region, location: location});
+  }
+
+
+  sendLocation = async() =>{
+    try {
+        const response = await fetch('https://friendrouter.xyz/api/location', {
+            method:'POST',
+            body: this.state.location,
+            headers: this.props.navigation.state.params.token
+        });
+        
+    } catch (error) {
+        console.error(error);
+    }
+    
+    this.props.navigation.navigate('Next', {token : this.state.responseJson.token});
+}
+
   render() {
     return (
       <View style={styles.container}>
-       <Text>{this.props.navigation.state.params.token} </Text>
+        <MapView style={styles.mapStyle}
+        region={this.state.region}
+        showUserLocation
+        showCompass={true}
+        rotateEnabled={false}
+        showsMyLocationButton={true}
+        >
+        <MapView.Marker
+        coordinate={{latitude: this.state.region.latitude,
+            longitude:this.state.region.longitude}}
+            title={"Zihan"}
+            description={"In class"}
+        >
+          
+        </MapView.Marker>
+
+        </MapView>
       </View>
     );
   }
@@ -17,5 +87,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
